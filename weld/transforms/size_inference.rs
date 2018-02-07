bbb59cb381989ae0e8346cf0637ca7690b83bf1c
Parimarjan Negi <pnegi@stanford.edu>
2018-02-06 23:13:51 -0800

    nditer length calculation just uses len = (end-start / stride)
    * removed checks for the iter being simple, as this should work fine for
    nditer as well
    * fixing issue with wrong call in automated build/testing script

--- weld/transforms/size_inference.rs
+++ weld/transforms/size_inference.rs
@@ -52,7 +52,7 @@ pub fn infer_size(expr: &mut Expr<Type>) {
         if let For { ref mut iters, ref mut builder, ref mut func } = expr.kind {
             /* Without this condition, the transform_up calls seems to end up in an infinite recursive loop */
             if let NewBuilder(None) = builder.kind {
-                if iters.len() > 0 && iters.iter().all(|ref iter| iter.is_simple()) && func_has_simple_merge(func) {
+                if iters.len() > 0 && func_has_simple_merge(func) {
                     if let Ident(_) = iters[0].data.kind {
                         /* Need the data_sym var after the length has been determined to update the
                          * expression. SymbolGenerator seems to borrow expression immutably, so we can't borrow it mutably before.*/
